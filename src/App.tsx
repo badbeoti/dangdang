045493a29bd15e74a0ecc2e86ff9d4f34f0c8c63
badbeoti <59817244+badbeoti@users.noreply.github.com>
2020-09-25 23:43:31 +0900

    데이터 셋팅 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import "./App.css";
 import * as d3 from "d3";
 import { select, selectAll } from "d3-selection";
 import data from "./data/data.json";
+import { stringify } from "querystring";
 
 interface DataProps {
 	data: typeof data;
@@ -10,11 +11,27 @@ interface DataProps {
 
 function DataList(props: DataProps) {
 	const { data } = props;
+	const [prevData, setData] = useState(props);
+	const newData = data.map((item) => {
+		const newObj = {
+			name: item.대여소명,
+			size: item.거치대수,
+			division: item.대여소_구,
+			id: item.대여소ID,
+		};
+		return newObj;
+	});
+
+	console.log(newData);
+	// 드디어... 데이터셋팅 완료....
 
 	return (
 		<div>
 			{data.map((item, index) => (
-				<li key={index}>{item.대여소명}</li>
+				<li key={index}>
+					{index}
+					{item.대여소_구}
+				</li>
 			))}
 		</div>
 	);
