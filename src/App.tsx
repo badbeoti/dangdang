f1b5a332a1c6f6abce2088a18a1479c2e6ffa771
badbeoti <59817244+badbeoti@users.noreply.github.com>
2020-09-25 17:52:09 +0900

    data 추가 d3 내장메소드 말고 그냥 import 하자 편안~~

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import "./App.css";
+import * as d3 from "d3";
+import { select, selectAll } from "d3-selection";
+import data from "./data/data.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	console.log(data);
+	const svgRef = useRef<SVGSVGElement | null>(null);
+	useEffect(() => {
+		console.log(select(svgRef.current));
+		select(svgRef.current)
+			.append("rect")
+			.attr("width", 100)
+			.attr("height", 100)
+			.attr("fill", "blue");
+	});
+
+	return (
+		<div>
+			<svg ref={svgRef} />
+		</div>
+	);
 }
 
 export default App;
