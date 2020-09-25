0adf00aee8c2c4d899c3bf64875ebd2760c3574c
badbeoti <59817244+badbeoti@users.noreply.github.com>
2020-09-25 22:23:57 +0900

    타입스크립트 너무 어렵다 난 정말 어렵다 어렵다 어렵다

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,41 @@ import * as d3 from "d3";
 import { select, selectAll } from "d3-selection";
 import data from "./data/data.json";
 
-function App() {
-	console.log(data);
-	const svgRef = useRef<SVGSVGElement | null>(null);
-	useEffect(() => {
-		console.log(select(svgRef.current));
-		select(svgRef.current)
-			.append("rect")
-			.attr("width", 100)
-			.attr("height", 100)
-			.attr("fill", "blue");
-	});
+interface DataProps {
+	data: typeof data;
+}
+
+function DataList(props: DataProps) {
+	const { data } = props;
 
 	return (
 		<div>
-			<svg ref={svgRef} />
+			{data.map((item, index) => (
+				<li key={index}>{item.대여소명}</li>
+			))}
 		</div>
+	);
+}
+
+function App() {
+	// console.log(data);
+	// const svgRef = useRef<SVGSVGElement | null>(null);
+	// useEffect(() => {
+	// 	console.log(select(svgRef.current));
+	// 	select(svgRef.current)
+	// 		.append("rect")
+	// 		.attr("width", 100)
+	// 		.attr("height", 100)
+	// 		.attr("fill", "blue");
+	// });
+
+	return (
+		<>
+			<DataList data={data}></DataList>
+		</>
+		// <div>
+		// 	<svg ref={svgRef} />
+		// </div>
 	);
 }
 
