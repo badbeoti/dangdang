026b0390e6afd7320d10c375e2cb356e53e8f9d0
badbeoti <59817244+badbeoti@users.noreply.github.com>
2020-09-30 21:56:11 +0900

    거치대수 사이즈 측정 필터링 함수

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,13 @@ import * as d3 from "d3";
 function App() {
 	console.log(newData);
 	const testFilter = d3.group(newData, (d) => d.division);
+	const gangnamSize = testFilter
+		.get("강남구")!
+		.map((data) => data.size)
+		.reduce((acc, cur) => acc + cur, 0);
+	console.log(testFilter.get("강남구"));
 	console.log(testFilter.get("강남구")!.length);
+	console.log(gangnamSize);
 	// 데이터 테스트필터링 구현 성공쓰
 
 	return (
