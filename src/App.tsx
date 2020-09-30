b7efb17e2bd071e5880616fb567b15bf2baa3906
badbeoti <59817244+badbeoti@users.noreply.github.com>
2020-09-30 20:04:58 +0900

    package update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useRef, useEffect, useState } from "react";
 import "./App.css";
 import * as d3 from "d3";
-import { select, selectAll } from "d3-selection";
 import DataList from "./component/DataList";
 import newData from "./data/newData";
 
