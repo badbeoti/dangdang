9b25c434fba9d04e735be17fa5c6c94bd89c5ebb
badbeoti <59817244+badbeoti@users.noreply.github.com>
2020-09-30 21:30:59 +0900

    데이터 테스트필터링 구현 성공쓰

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./App.css";
 import DataList from "./component/DataList";
 import newData from "./data/newData";
-import { group } from "d3-array";
+import * as d3 from "d3";
 
 function App() {
 	console.log(newData);
 	const testFilter = d3.group(newData, (d) => d.division);
-	console.log(testFilter);
+	console.log(testFilter.get("강남구")!.length);
+	// 데이터 테스트필터링 구현 성공쓰
 
 	return (
 		<>
