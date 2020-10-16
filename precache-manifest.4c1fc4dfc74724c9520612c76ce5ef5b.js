e9d37076b96800c9845e3e20c463f088a1b8a9b0
badbeoti <59817244+badbeoti@users.noreply.github.com>
2020-10-16 18:20:12 +0900

    Updates

---
+++ precache-manifest.4c1fc4dfc74724c9520612c76ce5ef5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62d01cde013359c5045b7e17e87a5cb2",
+    "url": "/dangdang/index.html"
+  },
+  {
+    "revision": "0f60f809fa771e252503",
+    "url": "/dangdang/static/css/main.22de92b1.chunk.css"
+  },
+  {
+    "revision": "1f6476814e4c57cf9be5",
+    "url": "/dangdang/static/js/2.7619ad7e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dangdang/static/js/2.7619ad7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f60f809fa771e252503",
+    "url": "/dangdang/static/js/main.6d893e91.chunk.js"
+  },
+  {
+    "revision": "1acfa3591e49e1fbade2",
+    "url": "/dangdang/static/js/runtime-main.4e685409.js"
+  }
+]);
